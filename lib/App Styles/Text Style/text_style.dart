b60e37a35b91499281cf1b6925527383ddf8b2b2
Aabro098 <arbinstha71@gmail.com>
2024-06-27 15:15:57 +0545

    setting up the router

--- lib/App Styles/Text Style/text_style.dart
+++ lib/App Styles/Text Style/text_style.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class TAppTextStyle{
-  
+  static TextStyle mainTitle = GoogleFonts.roboto(fontWeight: FontWeight.bold , fontSize : 18);
+  static TextStyle nameTitle = GoogleFonts.roboto(fontWeight: FontWeight.bold , fontSize : 14);
+  static TextStyle caption = GoogleFonts.nunito(fontWeight : FontWeight.w300 , fontSize : 12);
 }
---
+++ lib/Screens/Home Screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "/home";
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
